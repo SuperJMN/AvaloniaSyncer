b1ef72cf9db2256c10a0943f5993909b753eac11
José Manuel Nieto <superjmn@outlook.com>
2023-08-24 00:27:30 +0200

    Vamos por el buen camino

--- src/AvaloniaSyncer/ViewModels/SyncViewModel.cs
+++ src/AvaloniaSyncer/ViewModels/SyncViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 using AvaloniaSyncer.Plugins;
 using CSharpFunctionalExtensions;
@@ -9,12 +11,14 @@ using ReactiveUI;
 using ReactiveUI.Validation.Extensions;
 using Serilog;
 using Zafiro.Avalonia.Dialogs;
+using Zafiro.CSharpFunctionalExtensions;
 using Zafiro.UI;
 
 namespace AvaloniaSyncer.ViewModels;
 
 public class SyncViewModel : ViewModelBase
 {
+    private readonly SourceCache<SessionViewModel, string> bareSessions = new(x => x.Name);
     private readonly Maybe<ILogger> logger;
     private readonly INotificationService notificationService;
     private readonly SourceCache<SynchronizationViewModel, string> sessions = new(x => x.Title);
@@ -30,6 +34,13 @@ public class SyncViewModel : ViewModelBase
             .Bind(out var collection)
             .Subscribe();
 
+        bareSessions
+            .Connect()
+            .Bind(out var bareSessionsCollection)
+            .Subscribe();
+
+        BareSessions = bareSessionsCollection;
+
         Syncronizations = collection;
 
         CreateSyncSession = ReactiveCommand.CreateFromTask(async () =>
@@ -38,20 +49,20 @@ public class SyncViewModel : ViewModelBase
             Number++;
         });
 
-        SelectPlugins = ReactiveCommand.CreateFromTask(() => dialogService.ShowDialog(new SelectPluginsViewModel(pluginFactories), "Select plugins", new OptionConfiguration<SelectPluginsViewModel>("OK", context => ReactiveCommand.Create(() =>
-        {
-            context.Window.Close();
-            return new PluginSelection(context.ViewModel.Source!, context.ViewModel.Destination!);
-        }, context.ViewModel.IsValid()))));
+        SelectPlugins = ReactiveCommand.CreateFromTask(async () => { return await dialogService.Prompt("Select your plugins", new SelectPluginsViewModel(pluginFactories), "OK", vm => ReactiveCommand.Create(() => new PluginSelection(vm.Source!, vm.Destination!), vm.IsValid())); });
+
+        SelectPlugins.Values().Do(selection => bareSessions.AddOrUpdate(new SessionViewModel("Session", selection))).Subscribe();
     }
 
-    public ReactiveCommand<Unit, Unit> SelectPlugins { get; }
+    public ReadOnlyObservableCollection<SessionViewModel> BareSessions { get; set; }
+
+    public ReactiveCommand<Unit, Maybe<PluginSelection>> SelectPlugins { get; }
 
     public ReactiveCommand<Unit, Unit> CreateSyncSession { get; set; }
 
 
     public IEnumerable<SynchronizationViewModel> Syncronizations { get; }
-    
+
     private Task ShowDialog(IDialogService dialogService, IList<IPlugin> pluginFactories)
     {
         var vm = new CreateSyncSessionViewModel(pluginFactories);
@@ -74,6 +85,18 @@ public class SyncViewModel : ViewModelBase
 
         return dialogService.ShowDialog(vm, "Create  sync session", options);
     }
+}
+
+public class SessionViewModel
+{
+    public SessionViewModel(string name, PluginSelection selection)
+    {
+        Name = name;
+        Selection = selection;
+    }
+
+    public string Name { get; }
+    public PluginSelection Selection { get; }
 }
 
 public record PluginSelection(IPlugin Source, IPlugin Destination);
