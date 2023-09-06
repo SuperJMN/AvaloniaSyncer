65c2a91a3b55407fbf2853281e676fe8b9e35b42
José Manuel Nieto <superjmn@outlook.com>
2023-09-07 01:07:59 +0200

    Add ListBox temporarily

---
+++ src/AvaloniaSyncer/Sections/Synchronization/Sync/TopBar.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaSyncer.Sections.Synchronization.Sync
+{
+    public partial class TopBar : UserControl
+    {
+        public TopBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
