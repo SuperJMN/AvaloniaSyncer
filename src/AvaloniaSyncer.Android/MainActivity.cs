4d15f7f6127bb7322e8121c4ed7f80feb8e328ec
José Manuel Nieto <superjmn@outlook.com>
2023-10-21 20:17:16 +0200

    Half done

--- src/AvaloniaSyncer.Android/MainActivity.cs
+++ src/AvaloniaSyncer.Android/MainActivity.cs
@@ -1,8 +1,11 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
+using Android.OS;
 using Avalonia;
 using Avalonia.Android;
 using Avalonia.ReactiveUI;
+using Zafiro.FileSystem.Android;
 
 namespace AvaloniaSyncer.Android;
 
@@ -19,5 +22,17 @@ public class MainActivity : AvaloniaMainActivity<App>
         return base.CustomizeAppBuilder(builder)
             .WithInterFont()
             .UseReactiveUI();
+    }
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        AndroidFileSystem.Register(this);
+    }
+
+    protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+    {
+        base.OnActivityResult(requestCode, resultCode, data);
+        AndroidFileSystem.OnActivityResult(requestCode, resultCode, data);
     }
 }
