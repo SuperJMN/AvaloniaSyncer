3869f4d1f13e9f206f4c268a91341caa9bf12603
José Manuel Nieto <superjmn@outlook.com>
2023-11-13 11:52:36 +0100

    Cleanup

---
+++ build/DebPackages.cs
@@ -0,0 +1,59 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using DotnetPackaging.Common;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Serilog;
+
+class DebPackages
+{
+    public static async Task Create(Solution solution,
+        Configuration configuration,
+        AbsolutePath publishDirectory,
+        AbsolutePath outputDirectory,
+        string version)
+    {
+        var desktopProject = solution.AllProjects.First(project => project.Name.EndsWith("Desktop"));
+        var runtimes = new[] { "linux-x64", "linux-arm64" };
+
+        DotNetTasks.DotNetPublish(settings => DotNetPublishSettingsExtensions
+            .SetConfiguration(settings, configuration)
+            .SetProject(desktopProject)
+            .CombineWith(runtimes, (c, runtime) =>
+                c.SetRuntime(runtime)
+                    .SetOutput(publishDirectory / runtime)));
+
+        var results = await runtimes
+            .Select(runtime => Observable.FromAsync(async () =>
+            {
+                string projectName = solution.Name;
+                string architecture = runtime.Split("-")[1];
+
+                string packageName = $"{projectName!.Replace(" ", "").ToLower()}_{version}_{architecture}.deb";
+
+                var packageDefinition = await new FileInfo(solution.Directory / "metadata.deb.json").ToPackageDefinition();
+                
+                Log.Information("Creating {Package}", packageName);
+                var result = await DotnetPackaging.Create.Deb(packageDefinition, publishDirectory / runtime, outputDirectory / packageName);
+                return result
+                    .Finally(r =>
+                    {
+                        Log.Information("{Package} {Result}", packageName, r.Match(() => "Succeeded", error => $"Failed: {error}"));
+                        return r;
+                    });
+            }))
+            .Merge(1)
+            .ToList();
+
+        if (results.Combine().IsFailure)
+        {
+            throw new Exception(".deb creation failed");
+        }
+    }
+}
