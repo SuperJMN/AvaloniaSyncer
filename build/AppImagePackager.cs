96845164d7800593b35072e26890851a471d9303
José Manuel Nieto <superjmn@outlook.com>
2024-04-18 01:21:08 +0200

    Adapt to new DotnetPackaging

--- build/AppImagePackager.cs
+++ build/AppImagePackager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Abstractions;
@@ -66,6 +67,14 @@ public static class AppImagePackager
             },
             StartupWmClass = appName,
             Version = version,
+            AppId = "com.SuperJMN.AvaloniaSyncer",
+            HomePage = new Uri("https://github.com/SuperJMN/avaloniasyncer"),
+            License = "MIT",
+            ScreenshotUrls = new List<Uri>(
+                [
+                    new Uri("https://private-user-images.githubusercontent.com/3109851/294203061-da1296d3-11b0-4c20-b394-7d3425728c0e.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMzOTE4MjksIm5iZiI6MTcxMzM5MTUyOSwicGF0aCI6Ii8zMTA5ODUxLzI5NDIwMzA2MS1kYTEyOTZkMy0xMWIwLTRjMjAtYjM5NC03ZDM0MjU3MjhjMGUucG5nP1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9QUtJQVZDT0RZTFNBNTNQUUs0WkElMkYyMDI0MDQxNyUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNDA0MTdUMjIwNTI5WiZYLUFtei1FeHBpcmVzPTMwMCZYLUFtei1TaWduYXR1cmU9MGUwNzVjMGUwMmIyNjMzYjRjNDJlODRjM2U2M2YzYjU1ZDkxNmMwYTc4YzU3NGVmYTViNDBkNDYzZGE3ZTUzNCZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QmYWN0b3JfaWQ9MCZrZXlfaWQ9MCZyZXBvX2lkPTAifQ.jwDV9Bh-bRoqHCeo1DqKCMebdVdPVIS-dBuBGjk_dE8")
+                ]),
+            Summary = "This is an application to rule every filesystem"
         };
                 
         await AppImage.WriteFromBuildDirectory(output, inputDir, metadata);
