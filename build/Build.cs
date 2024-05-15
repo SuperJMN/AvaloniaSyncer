5a611c7891df3f928ce18b118985eda9ed0b5db1
José Manuel Nieto <superjmn@outlook.com>
2024-05-15 11:09:35 +0200

    Format

--- build/Build.cs
+++ build/Build.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using CSharpFunctionalExtensions;
 using DotnetPackaging;
 using Nuke.Common;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tools.GitVersion;
-using Nuke.Common.Git;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using Serilog;
 using Zafiro.Mixins;
 using Zafiro.Nuke;
@@ -17,33 +17,32 @@ using Maybe = CSharpFunctionalExtensions.Maybe;
 
 class Build : NukeBuild
 {
-    public static int Main() => Execute<Build>(x => x.PublishGitHubRelease);
+    [Parameter("The alias for the key in the keystore.")] readonly string AndroidSigningKeyAlias;
+    [Parameter("The password of the key within the keystore file.")] [Secret] readonly string AndroidSigningKeyPass;
+    [Parameter("The password for the keystore file.")] [Secret] readonly string AndroidSigningStorePass;
+    [Parameter("Contents of the keystore encoded as Base64.")] readonly string Base64Keystore;
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")] readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter("GitHub Authentication Token")] [Secret] readonly string GitHubAuthenticationToken;
+    [GitVersion] readonly GitVersion GitVersion;
+    [GitRepository] readonly GitRepository Repository;
 
     public AbsolutePath OutputDirectory = RootDirectory / "output";
 
     [Solution] Solution Solution;
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")] readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
-    [GitVersion] readonly GitVersion GitVersion;
-    [GitRepository] readonly GitRepository Repository;
-
-    [Parameter("GitHub Authentication Token")][Secret] readonly string GitHubAuthenticationToken;
-    [Parameter("Contents of the keystore encoded as Base64.")] readonly string Base64Keystore;
-    [Parameter("The alias for the key in the keystore.")] readonly string AndroidSigningKeyAlias;
-    [Parameter("The password for the keystore file.")][Secret] readonly string AndroidSigningStorePass;
-    [Parameter("The password of the key within the keystore file.")][Secret] readonly string AndroidSigningKeyPass;
-    
-    private Actions Actions { get; }
 
     public Build()
     {
         Actions = new Actions(Solution, Repository, RootDirectory, GitVersion, Configuration);
     }
 
+    Actions Actions { get; }
+
     Target Clean => td => td
         .Executes(() =>
         {
             OutputDirectory.CreateOrCleanDirectory();
-            var absolutePaths = RootDirectory.GlobDirectories("**/bin", "**/obj").Where(a => !((string)a).Contains("build")).ToList();
+            var absolutePaths = RootDirectory.GlobDirectories("**/bin", "**/obj").Where(a => !((string) a).Contains("build")).ToList();
             Log.Information("Deleting {Dirs}", absolutePaths);
             absolutePaths.DeleteDirectories();
         });
@@ -54,7 +53,6 @@ class Build : NukeBuild
             DotNetWorkloadRestore(x => x.SetProject(Solution));
         });
 
-    
     Target PublishGitHubRelease => td => td
         .DependsOn(RestoreWorkloads)
         .OnlyWhenStatic(() => Repository.IsOnMainOrMasterBranch())
@@ -72,6 +70,8 @@ class Build : NukeBuild
                 .TapError(e => throw new ApplicationException(e));
         });
 
+    public static int Main() => Execute<Build>(x => x.PublishGitHubRelease);
+
     Options Options()
     {
         IEnumerable<AdditionalCategory> additionalCategories = [AdditionalCategory.FileTransfer, AdditionalCategory.FileTools, AdditionalCategory.FileManager, AdditionalCategory.Filesystem];
@@ -81,7 +81,7 @@ class Build : NukeBuild
             new Uri("https://private-user-images.githubusercontent.com/3109851/294203061-da1296d3-11b0-4c20-b394-7d3425728c0e.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTU3NjEyMzQsIm5iZiI6MTcxNTc2MDkzNCwicGF0aCI6Ii8zMTA5ODUxLzI5NDIwMzA2MS1kYTEyOTZkMy0xMWIwLTRjMjAtYjM5NC03ZDM0MjU3MjhjMGUucG5nP1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9QUtJQVZDT0RZTFNBNTNQUUs0WkElMkYyMDI0MDUxNSUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNDA1MTVUMDgxNTM0WiZYLUFtei1FeHBpcmVzPTMwMCZYLUFtei1TaWduYXR1cmU9MjBhZDc5YzlhM2ZlMTBmZDc4ODcwZjBjMmJiYzU0Zjc2ZTMyMjAwYjgwZmE1NmZlYzBhYmM2MjIwOTBjMWVmZSZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QmYWN0b3JfaWQ9MCZrZXlfaWQ9MCZyZXBvX2lkPTAifQ.3V2PtKWDyqZqw5AUZMxkt5Dh6k7xa8-eNre7hzjR-lI")
         ];
 
-        return new Options()
+        return new Options
         {
             MainCategory = MainCategory.Utility,
             AdditionalCategories = Maybe.From(additionalCategories),
@@ -105,7 +105,7 @@ class Build : NukeBuild
             },
             License = "MIT",
             ScreenshotUrls = Maybe.From(screenShots),
-            Summary = "This is an application to rule every filesystem",
+            Summary = "This is an application to rule every filesystem"
         };
     }
 }
